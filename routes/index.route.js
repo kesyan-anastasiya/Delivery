2b69415ecd117faaa0af745b82fa0350fa16864d
kobikni <nik.kobyakov.80@mail.ru>
2024-03-21 12:18:32 +0300

    main_str

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,14 +1,14 @@
-// const router = require('express').Router()
+const router = require('express').Router()
 // const usersRouter = require('./views/users.routes').Router()
-// const mainRouter = require('./views/main.routes').Router()
+const mainRouter = require('./views/main.routes')
 // const ordersRouter = require('./views/orders.routes').Router()
 // const mainApiRouter = require('./api/api.main.route')
 
-// router.use('/', mainRouter)
+router.use('/', mainRouter)
 // router.use('/orders', ordersRouter)
 // router.use('/users', usersRouter)
 // router.use('/api', mainApiRouter)
 
 
 
-// module.exports = router;
+module.exports = router;
