a62f8fb9e0c5c7be50e4e1eeb9cb7de5434b57bf
kobikni <nik.kobyakov.80@mail.ru>
2024-03-22 14:33:05 +0300

    delet22

--- components/ui/Cards.jsx
+++ components/ui/Cards.jsx
@@ -1,16 +1,18 @@
 const React = require('react')
 
-function Cards({ carts, user }) {
+function Cards({ carts, user }) { 
     // console.log(user.status);
     return (
-        <div className="order_container">
+<div className="order_container">
             {carts.map((cart) => (
+                
                 <div key={cart.id} className="card" style={{ width: '25rem' }}>
                     <img
                         className="card-img-top"
                         src={cart.img}
                         style={{ width: '300px', height: '250px' }}
                     />
+                    
 
                     <div className="card-body">
                         <div className="card-title">{cart.name}</div>
@@ -28,28 +30,27 @@ function Cards({ carts, user }) {
                                 Описание заказа
                             </button>
                         </a>
-
-                        <a href={`/orders/buy/${cart.id}`}>
-                            <button className="btn btn-success">
-                                Выкупить
-                            </button>
+                        { (user?.status === 'lox' &&  (
+                           <>  
+                             <button className="btn btn-success">
+                             Выкупить<a href=""></a>
+                         </button>
+                         </>
+                         ))}
+                       
+                { (user?.status === 'Admin' &&  (  
+                <><a
+                            className="btn btn-secondary"
+                            href={`/orders/${cart.id}`}
+                        >
+                            Изменить
                         </a>
-                        {user?.status === 'Admin' && (
-                            <>
-                                <a
-                                    className="btn btn-secondary"
-                                    href={`/orders/${cart.id}`}
-                                >
-                                    Изменить
-                                </a>
-                                <button
-                                    className="btn btn-secondary delete"
-                                    data-id={cart.id}
-                                >
-                                    Удалить
-                                </button>
-                            </>
-                        )}
+                        <button
+                            className="btn btn-secondary delete"
+                            data-id={cart.id}
+                        >
+                            Удалить
+                        </button></>))}
                     </div>
                 </div>
             ))}
