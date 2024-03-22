3e28c5f6ba8813b5643c57286d1db8ff5e3951fc
kobikni <nik.kobyakov.80@mail.ru>
2024-03-22 14:31:55 +0300

    delet222

---
+++ public/scripts/delete.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.order_container')
+if (container) {
+container.addEventListener('click',async (e)=>{
+if(e.target.classList.contains('delete')){
+    const agree = confirm("Удалить?")
+    if(agree === true){
+    const {id}= e.target.dataset
+   const res = await fetch(`/api/main/${id}`, {
+       method: 'DELETE',
+   })
+   const data = await res.json()
+   if(data.message === 'ok'){
+    e.target.closest('.card').remove()
+   }
+}
+}
+})
+}
